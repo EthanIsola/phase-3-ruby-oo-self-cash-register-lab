b737093a27d044d4ff08e72b52501dd2e8cd5474
Ethan Isola <isolaethan@gmail.com>
2022-02-24 11:42:54 -0600

    commit tree 2119bc4a25bf0abce0cb78ba14e0be17d5eba65b

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total, :items, :transactions
+    def initialize(employee_discount = 0)
+        @total = 0
+        @discount = employee_discount
+        @items = []
+        @transactions = []
+    end
 
+    def add_item(title, price, quantity = 1)
+        self.total += (price * quantity)
+        self.transactions << (price * quantity)
+        quantity.times do
+            self.items << title
+        end
+    end
+
+    def apply_discount
+        if self.discount != 0
+            self.total = self.total - self.discount*10
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        if self.transactions.length != 0
+            self.total -= self.transactions[-1]
+            self.transactions.pop
+        else
+            "No recent transactions"
+        end
+    end
+    
+end
